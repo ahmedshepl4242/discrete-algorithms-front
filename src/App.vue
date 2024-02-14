837b52d5d68162551603a88e23c3e1996a582bc5
Ahmed samir al saed <119258203+ahmedshepl4242@users.noreply.github.com>
2024-02-15 00:51:50 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,9 @@ import { RouterLink, RouterView } from "vue-router";
     </nav>
   </header>
 
-  <RouterView />
+   <div class="router">
+      <RouterView />
+    </div>
 </template>
 
 <style scoped>
